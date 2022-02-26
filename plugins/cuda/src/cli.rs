20f80ae1472560d483e46874abfffee1dfec5359
Ben Rod <mozaiti@gmail.com>
2022-02-27 03:29:25 +0800

    Add nvml-wrapper to get lcc/lmc work

--- plugins/cuda/src/cli.rs
+++ plugins/cuda/src/cli.rs
@@ -1,8 +1,8 @@
 #[derive(clap::Args, Debug)]
 pub struct CudaOpt {
-    #[clap(long = "cuda-device", use_delimiter = true, help = "Which CUDA GPUs to use [default: all]")]
+    #[clap(long = "cuda-device", use_delimiter = true, help = "Which CUDA GPUs to use eg: 0,1[default: all]")]
     pub cuda_device: Option<Vec<u16>>,
-    #[clap(long = "cuda-workload", help = "Ratio of nonces to GPU possible parrallel run [defualt: 16]")]
+    #[clap(long = "cuda-workload", help = "Ratio of nonces to GPU possible parrallel run [default: 16]")]
     pub cuda_workload: Option<Vec<f32>>,
     #[clap(
         long = "cuda-workload-absolute",
@@ -13,4 +13,8 @@ pub struct CudaOpt {
     pub cuda_disable: bool,
     #[clap(long = "cuda-blocking-sync", help = "Block threads when waiting for GPU result. Lowers CPU usage, but might cause delays resulting in red blocks. Requires higher workload.")]
     pub cuda_blocking_sync: bool,
+    #[clap(long = "cuda-lock-mem-clocks", use_delimiter = true, help = "Lock mem clocks eg: ,810, [default: 0]")]
+    pub cuda_lock_mem_clocks: Option<Vec<u32>>,
+    #[clap(long = "cuda-lock-core-clocks", use_delimiter = true, help = "Lock core clocks eg: ,1200, [default: 0]")]
+    pub cuda_lock_core_clocks: Option<Vec<u32>>,
 }
